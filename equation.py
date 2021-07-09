ba5f8b327dda55a97f3cd766f5f7778577178ed9
Karun Kris <34982169+kk1708@users.noreply.github.com>
2021-07-09 10:27:10 +0530

    Code for solving the returned string

---
+++ equation.py
@@ -0,0 +1,37 @@
+from sympy import solve
+from sympy import sympify
+from sympy.abc import F, x, y, z, a, b
+from sympy.parsing.sympy_parser import parse_expr
+
+
+if "x" in s:
+    try:
+        equation = s.replace('--','=')
+        a = []
+        for i in range(0,len(equation)):
+            if i==0:
+                continue
+            if equation[i] == 'x':
+                if equation[i-1] == '+' or equation[i-1] == '-':
+                    continue
+                else:
+                    a.append(i)
+
+        counts = 0
+        for i in a:
+            if counts == 0:
+                counts=1
+                equation = equation[:i] + '*' + equation[i:]
+            else:
+                i=i+counts
+                equation = equation[:i] + '*' + equation[i:]
+                counts+=1
+
+        lhs =  parse_expr(equation.split("=")[0])
+        rhs =  parse_expr(equation.split("=")[1])
+        solution = solve(lhs-rhs)
+        print(solution[0])
+    except:
+        print("invalid equation")
+else:
+    print(eval(s))
